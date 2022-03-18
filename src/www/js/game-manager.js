9a6e218920f4f6d233b586dad5f73e4cac6f9d76
Mr Josh <mr-josh@sparklapse.com>
2022-03-19 01:33:18 +1100

    Added leaderboard to the side

--- src/www/js/game-manager.js
+++ src/www/js/game-manager.js
@@ -1,6 +1,11 @@
+const API = "https://memory.edgeless.land/api";
+
 window.addEventListener("DOMContentLoaded", () => {
     // Variables
     let menu = document.querySelector("#menu");
+    let initials_input = document.querySelector("#initials");
+    let leaderboard = document.querySelector("#leaderboard");
+    let leaderboard_scores = document.querySelector("#leaderboard-scores");
     let game_board = document.querySelector("memory-board");
     let lives_div = document.querySelector("#lives");
     let score_text = document.querySelector("#score");
@@ -11,10 +16,12 @@ window.addEventListener("DOMContentLoaded", () => {
     let score = 0;
     let round = 0;
 
+    let initials = "unkn";
+
     let difficulty = {
         scale: 3,
         size: 3,
-        size_max: 7,
+        size_max: 6,
         answers: 3,
         answers_max: 20,
         preview_time: 750,
@@ -22,7 +29,7 @@ window.addEventListener("DOMContentLoaded", () => {
     let difficulty_defaults = {
         scale: 3,
         size: 3,
-        size_max: 7,
+        size_max: 6,
         answers: 3,
         answers_max: 20,
         preview_time: 750,
@@ -34,6 +41,7 @@ window.addEventListener("DOMContentLoaded", () => {
         score_text.innerText = score;
     }
 
+    // Reset game state
     function initGame() {
         lives = max_lives;
         round = 0;
@@ -62,50 +70,112 @@ window.addEventListener("DOMContentLoaded", () => {
         }
     }
 
+    async function refreshLeaderboard() {
+        leaderboard_scores.innerHTML = "";
+        let req = await fetch(API + "/leaderboard/?amount=10");
+        let scores = await req.json();
+
+        for (const score of scores) {
+            let item = document.createElement("li");
+            item.innerText = `${score.user.initials} - ${score.score}`;
+
+            leaderboard_scores.appendChild(item);
+        }
+    }
+
+    // Show the game board and related stats
     function gameScreen() {
         menu.classList.add("hidden");
+        leaderboard.classList.add("hidden");
         score_text.classList.remove("hidden");
         lives_div.classList.remove("hidden");
         game_board.classList.remove("hidden");
     }
 
+    // Show the menu
     function menuScreen() {
+        refreshLeaderboard();
+
         menu.classList.remove("hidden");
+        initials_input.value = "";
+        leaderboard.classList.remove("hidden");
         score_text.classList.add("hidden");
         lives_div.classList.add("hidden");
         game_board.classList.add("hidden");
     }
 
-    // Logic
+    function menuStart() {
+        // Get initials
+        if (initials_input.value.length != 4) {
+            alert("Please enter 4 letters for your initials.");
+            return;
+        }
 
-    // == Event listeners
-    menu.querySelector("#start-game").addEventListener("click", () => {
-        gameScreen();
+        initials = initials_input.value;
+        // Reset the game state
         initGame();
+        // Show the game board
+        gameScreen();
+    }
+
+    // Start the game on button click
+    menu.querySelector("#start-game").addEventListener("click", () => {
+        menuStart();
+    });
+    initials_input.addEventListener("keypress", (e) => {
+        if (e.keyCode != 13) return;
+        menuStart();
     });
 
+
+    // Listen for every time the player presses an incorrect answer
     game_board.addEventListener("card-incorrect", () => {
+        // Remove a life for pressing an incorrect card
         lives--;
+
+        // If game over
         if (lives === 0) {
+            // Lock the game board and show the answers
             game_board.lock();
             game_board.showAnswers();
 
-            setTimeout(() => {
+            // Do a short delay before returning to the menu
+            setTimeout(async () => {
+                // Submit the score to the leaderboard
+                fetch(
+                    `${API}/leaderboard/?score=${score}&initials=${initials}`,
+                    {
+                        method: "PUT",
+                    }
+                )
+                .then(() => {
+                    refreshLeaderboard();
+                })
+                .catch((err) => {
+                    console.err(err);
+                });
+
+                // Return to menu
                 menuScreen();
             }, 1500);
         }
 
+        // Update the lives icons
         lives_icons[lives].classList.add("lost");
         lives_icons[lives].innerHTML = "heart_broken";
     })
 
+    // Listen for every time the player presses a correct answer
     game_board.addEventListener("card-correct", () => {
         updateScore(1);
     });
 
+    // Listen for every time the player finishes a round/board
     game_board.addEventListener("game-won", () => {
+        // Give extra points for finishing a round
         updateScore(difficulty.size + difficulty.answers);
 
+        // Increase the difficulty
         round += 1;
         if (round % 3 === 0) {
             round += 1;
@@ -120,6 +190,7 @@ window.addEventListener("DOMContentLoaded", () => {
             }
         }
 
+        // Start the next round after a short delay
         setTimeout(() => {
             game_board.createGame(difficulty.size, difficulty.answers);
             setTimeout(() => {
@@ -128,5 +199,6 @@ window.addEventListener("DOMContentLoaded", () => {
         }, 250);
     });
 
+    // Show the menu on page load
     menuScreen();
 });
