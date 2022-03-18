21ceac8ddb61005f3548437cd32c23e76e23f2ee
Mr Josh <mr-josh@sparklapse.com>
2022-03-18 16:41:11 +1100

    Initial commit
    Got basic memory game going with a basic menu to restart

---
+++ src/www/components/memory-board.js
@@ -0,0 +1,208 @@
+class MemoryBoard extends HTMLElement {
+    // DOM
+    #shadow;
+
+    // Game board
+    #size = -1;
+    #cards = [];
+    #answers = [];
+    #picked = [];
+
+    // Logic
+    #locked = false;
+
+    // Style
+    options = {
+        "card-color": "white",
+        "card-color-correct": "green",
+        "card-color-incorrect": "red",
+    };
+
+    constructor() {
+        super();
+        this.#shadow = this.attachShadow({ mode: "closed" });
+    }
+
+    static get observedAttributes() {
+        return ["card-color", "card-color-correct", "card-color-incorrect"];
+    }
+
+    attributeChangedCallback(name, _, newValue) {
+        switch (name) {
+            case "card-color":
+                this.options["card-color"] = newValue;
+                break;
+            case "card-color-correct":
+                this.options["card-color-correct"] = newValue;
+                break;
+            case "card-color-incorrect":
+                this.options["card-color-incorrect"] = newValue;
+                break;
+        }
+    }
+
+    #pickRandomCards(answersAmount, maxCards) {
+        let cards = [];
+        for (let i = 0; i < maxCards; i++) {
+            cards.push(i);
+        }
+
+        let answers = [];
+        for (let i = 0; i < answersAmount; i++) {
+            let index = Math.floor(Math.random() * cards.length);
+            answers.push(cards[index]);
+            cards = cards.filter(card => card !== cards[index]);
+        }
+
+        return answers;
+    }
+
+    #handleCardClick(index) {
+        let e = new Event("card-click");
+        this.dispatchEvent(e);
+
+        if (this.#locked) return;
+        if (this.#picked.includes(index)) return;
+
+        if (this.#answers.includes(index)) {
+            let e = new Event("card-correct");
+            this.dispatchEvent(e);
+
+            this.#cards[index].classList.add("correct");
+
+            this.#picked.push(index);
+            this.#picked.sort();
+        } else {
+            let e = new Event("card-incorrect");
+            this.dispatchEvent(e);
+
+            this.#cards[index].classList.add("incorrect");
+        }
+
+        if (this.#picked.length === this.#answers.length && this.#picked.every((value, index) => value === this.#answers[index])) {
+            let e = new Event("game-won");
+            this.dispatchEvent(e);
+
+            this.#locked = true;
+        }
+    }
+
+    #resetGame() {
+        this.#size = -1;
+        this.#cards = [];
+        this.#answers = [];
+        this.#picked = [];
+        this.#locked = false;
+    }
+
+    createGame(size, answers) {
+        let e = new Event("game-start");
+        this.dispatchEvent(e);
+
+        if (answers > size * size || answers < 1) {
+            throw new Error("Active amount must be between 1 and the total amount of cards");
+        }
+
+        this.#resetGame();
+
+        this.#size = size;
+
+        this.#shadow.innerHTML = /* css */`
+            <style>
+                :host {
+                    display: grid;
+                    grid-template-columns: repeat(${size}, 1fr);
+                    grid-template-rows: repeat(${size}, 1fr);
+                    grid-gap: 1rem;
+                    aspect-ratio: 1;
+                }
+
+                .card {
+                    background-color: ${this.options["card-color"]};
+                    border: none;
+                    border-radius: 0.5rem;
+                    box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.4);
+                    transition: 0.2s;
+                }
+
+                .card.correct {
+                    background-color: ${this.options["card-color-correct"]};
+                }
+
+                .card.incorrect {
+                    background-color: ${this.options["card-color-incorrect"]};
+                }
+
+                .card:hover {
+                    cursor: pointer;
+                    transform: scale(1.1);
+                    box-shadow: 0 0 0.8rem rgba(0, 0, 0, 0.2);
+                }
+            </style>
+        `;
+
+        let pickedCardIndices = this.#pickRandomCards(answers, size * size);
+        let cards = [];
+        for (let i = 0; i < size * size; i++) {
+            let card = document.createElement("button");
+            card.setAttribute("class", "card");
+            card.setAttribute("id", i);
+
+            card.classList.add("card");
+            card.addEventListener("click", this.#handleCardClick.bind(this, i));
+
+            cards.push(card);
+            this.#shadow.appendChild(card);
+        }
+
+        this.#cards = cards;
+        this.#answers = pickedCardIndices;
+        this.#answers.sort();
+    }
+
+    previewAnswers(duration = 1000) {
+        let e = new Event("previewing");
+        this.dispatchEvent(e);
+
+        this.#locked = true;
+
+        for (let card of this.#cards) {
+            if (this.#answers.includes(parseInt(card.id))) {
+                card.classList.add("correct");
+
+                setTimeout(() => {
+                    card.classList.remove("correct");
+                }, duration);
+            }
+        }
+
+        setTimeout(() => {
+            this.#locked = false;
+        }, duration + 100);
+    }
+
+    showAnswers() {
+        for (let card of this.#cards) {
+            if (this.#answers.includes(parseInt(card.id))) {
+                card.classList.add("correct");
+            }
+        }
+    }
+
+    clearAnswers() {
+        for (let card of this.#cards) {
+            card.classList.remove("correct");
+            card.classList.remove("incorrect");
+        }
+    }
+
+    lock() {
+        this.#locked = true;
+    }
+
+    get size() {
+        return this.#size;
+    }
+}
+
+window.customElements.define("memory-board", MemoryBoard);
---
+++ src/www/js/game-manager.js
@@ -0,0 +1,129 @@
+window.addEventListener("DOMContentLoaded", () => {
+    // Variables
+    let menu = document.querySelector("#menu");
+    let game_board = document.querySelector("memory-board");
+    let lives_div = document.querySelector("#lives");
+    let score_text = document.querySelector("#score");
+
+    let lives_icons = [];
+    let lives = 3;
+    let max_lives = 3;
+    let score = 0;
+
+    let round = 0;
+    let difficulty = {
+        scale: 3,
+        size: 3,
+        size_max: 7,
+        answers: 3,
+        answers_max: 20,
+        preview_time: 750,
+    };
+    let difficulty_defaults = {
+        scale: 3,
+        size: 3,
+        size_max: 7,
+        answers: 3,
+        answers_max: 20,
+    };
+    // Functions
+    function updateScore(change) {
+        score += change;
+        score_text.innerText = score;
+    }
+
+    function initGame() {
+        lives = max_lives;
+        round = 0;
+        difficulty = {
+            ...difficulty_defaults,
+        };
+        score = 0;
+        updateScore(0);
+
+        game_board.createGame(difficulty.size, difficulty.answers);
+        setTimeout(() => {
+            game_board.previewAnswers(difficulty.preview_time);
+        }, 100);
+    
+        let icons = document.querySelector("#lives");
+        icons.innerHTML = "";
+        for (let i = 0; i < lives; i++) {
+            let icon = document.createElement("span");
+            icon.classList.add("material-icons");
+            icon.classList.add("life");
+            icon.innerText = "favorite";
+    
+            lives_icons.push(icon);
+            icons.appendChild(icon);
+        }
+    }
+
+    function gameScreen() {
+        menu.classList.add("hidden");
+        score_text.classList.remove("hidden");
+        lives_div.classList.remove("hidden");
+        game_board.classList.remove("hidden");
+    }
+    
+    function menuScreen() {
+        menu.classList.remove("hidden");
+        score_text.classList.add("hidden");
+        lives_div.classList.add("hidden");
+        game_board.classList.add("hidden");
+    }
+
+    // Logic
+
+    // == Event listeners
+    menu.querySelector("#start-game").addEventListener("click", () => {
+        gameScreen();
+        initGame();
+    });
+
+    game_board.addEventListener("card-incorrect", () => {
+        lives--;
+        if (lives === 0) {
+            game_board.lock();
+            game_board.showAnswers();
+
+            setTimeout(() => {
+                menuScreen();
+            }, 1500);
+        }
+
+        lives_icons[lives].classList.add("lost");
+        lives_icons[lives].innerHTML = "heart_broken";
+    })
+
+    game_board.addEventListener("card-correct", () => {
+        updateScore(1);
+    });
+
+    game_board.addEventListener("game-won", () => {
+        updateScore(difficulty.size + difficulty.answers);
+
+        round += 1;
+        if (round % 3 === 0) {
+            round += 1;
+            difficulty.scale += 2;
+
+            if (difficulty.size < difficulty.size_max) {
+                difficulty.size += 1;
+            }
+
+            if (difficulty.answers < difficulty.answers_max) {
+                difficulty.answers += 2 + Math.max(0, difficulty.scale - 5);
+            }
+        }
+
+        setTimeout(() => {
+            game_board.createGame(difficulty.size, difficulty.answers);
+            setTimeout(() => {
+                game_board.previewAnswers(difficulty.preview_time);
+            }, 100);
+        }, 250);
+    });
+
+    menuScreen();
+});
